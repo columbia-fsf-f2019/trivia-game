7f895be6ea8365897b9e73a88f022a6b139fef75
Tuscany <lizlevine.rigie@gmail.com>
2019-11-23 01:38:04 -0500

    basic html build out, added some css design features, named game

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+
+  
+  
+    var questions = [
+            {
+                text: C"apital of ct?"
+                choices: ["new Haven", "hartford", etc..."
+
+        ]
+         var curQuestion = 0;
+         var timeLeft = 20;
+         var questionTimer = setInterval(function ())
+
+         timeLeft --;
+         if (timeLeft <= 0) {
+             curQuestion ++;
+             rendertiesUpMessage();
+             setTimeout(function() {
+                 renderQuewtion(questions[curQuestion }, 3000);
+                 ])
+             }
+         }
