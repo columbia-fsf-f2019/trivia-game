655004416c3440cc893d9ade574e2c5c2b632eef
Tuscany <lizlevine.rigie@gmail.com>
2019-11-23 17:51:22 -0500

    wrote 11 questions, choices and answers for game in js as an object array

---
+++ assets/javascript/app-no.js
@@ -0,0 +1,93 @@
+$("#start").on("click", start);
+
+document.getElementById("start").innerHTML = 003 + ":" + 20;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById("start").innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond(timeArray[1] - 1);
+  if (s == 59) {
+    m = m - 1;
+  }
+  //if(m<0){alert('timer completed')}
+
+  document.getElementById("start").innerHTML = m + ":" + s;
+  console.log(m);
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec;
+  } // add zero in front of numbers < 10
+  if (sec < 0) {
+    sec = "59";
+  }
+  return sec;
+}
+
+// ______________________________________________________________________________
+
+// $("#start").on("click", function() {
+
+// var number = 240;
+//  Variable that will hold our interval ID when we execute
+//  the "run" function
+// var intervalId;
+// //  When the stop button gets clicked, run the stop function.
+// $("#start").on("click", start);
+//  When the resume button gets clicked, execute the run function.
+// $("#resume").on("click", run);
+//  The run function sets an interval
+//  that runs the decrement function once a second.
+//  *****BUG FIX********
+//  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+// function run() {
+//   clearInterval(intervalId);
+//   intervalId = setInterval(decrement, 2400);
+// }
+//  The decrement function.
+// function decrement() {
+//  Decrease number by one.
+//   number--;
+//  Show the number in the #show-number tag.
+//   $("#show-number").html("<h2>" + number + "</h2>");
+//   //  Once number hits zero...
+//   if (number === 0) {
+//  ...run the stop function.
+// stop();
+//  Alert the user that time is up.
+//     alert("Time Up!");
+//   }
+// }
+// //  The stop function
+// function stop() {
+//  Clears our intervalId
+//  We just pass the name of the interval
+//   //  to the clearInterval function.
+//   clearInterval(intervalId);
+// }
+//  Execute the run function.
+// run();
+
+// var questions = [
+//         {
+//             text: C"apital of ct?"
+//             choices: ["new Haven", "hartford", etc..."
+
+//     ]
+//      var curQuestion = 0;
+//      var timeLeft = 20;
+//      var questionTimer = setInterval(function ())
+
+//      timeLeft --;
+//      if (timeLeft <= 0) {
+//          curQuestion ++;
+//          rendertiesUpMessage();
+//          setTimeout(function() {
+//              renderQuewtion(questions[curQuestion }, 3000);
+//              ])
+//          }
+//      }
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,106 @@
+// Questions object
 
-  
-  
-    var questions = [
-            {
-                text: C"apital of ct?"
-                choices: ["new Haven", "hartford", etc..."
-
-        ]
-         var curQuestion = 0;
-         var timeLeft = 20;
-         var questionTimer = setInterval(function ())
-
-         timeLeft --;
-         if (timeLeft <= 0) {
-             curQuestion ++;
-             rendertiesUpMessage();
-             setTimeout(function() {
-                 renderQuewtion(questions[curQuestion }, 3000);
-                 ])
-             }
-         }
+var questions = [
+  {
+    question:
+      "Which type of American grape is equivalent to the Italian Primitivo grape?",
+    choices: [
+      "Cabernet Sauvignon",
+      "Merlot",
+      "Zinfandel",
+      "Pinot Noir",
+      "Petite Syrah"
+    ],
+    answer: "Zinfandel"
+  },
+  {
+    question: "Which of the following red varietals are NOT from Tuscany?",
+    choices: [
+      "Chianti",
+      "Primitivo",
+      "Sangiovese",
+      "Canaiolo Nero",
+      "Brunello di Montalcino"
+    ],
+    answer: "Primitivo"
+  },
+  {
+    question:
+      "Which of the following wood species is NOT used to make barrels for aging wine?",
+    choices: [
+      "American Oak",
+      "French Oak",
+      "European Beachwood",
+      "Hungarian Oak",
+      "California Redwood"
+    ],
+    answer: "European Beachwood"
+  },
+  {
+    question: "Which appelation is NOT located in France?",
+    choices: [
+      "Loire Valley",
+      "Cotes du Rhone",
+      "Bordeaux",
+      "Rioja",
+      "Burgundy",
+      "Sancerre"
+    ],
+    answer: "Rioja"
+  },
+  {
+    question:
+      "Which legendary French winemaker put Napa Valley 'on-the-map' as a world-class wine producer?",
+    choices: [
+      "Robert Mondavi",
+      "Rodney Strong",
+      "Charles Krug",
+      "Georges de Latour",
+      "André Tchelistcheff"
+    ],
+    answer: "Georges de Latour"
+  },
+  {
+    question:
+      "Which American appelation is best known for producing a dry and 'dusty' red grape?",
+    choices: [
+      "Calistoga",
+      "Los Carneros",
+      "Rutherford",
+      "Stags Leap District",
+      "Sonoma"
+    ],
+    answer: "Rutherford"
+  },
+  {
+    question: "Which grape is looked down upon as being 'inferior'?",
+    choices: ["Merlot", "Sauvignon Blanc", "Chardonnay", "Cabernet Sauvignon"],
+    answer: "Chardonnay"
+  },
+  {
+    question: "Which description is generally NOT used to describe wine?",
+    choices: [
+      "dry",
+      "fruit-forward",
+      "tanic",
+      "bold",
+      "peppery",
+      "savory",
+      "earthy",
+      "barnyardy"
+    ],
+    answer: "savory"
+  },
+  {
+    question:
+      "Which country is the largest producer of the Sauvignon Blanc grape?",
+    choices: ["France", "Italy", "Australia", "United States", "South Africa"],
+    answer: "France"
+  },
+  {
+    question:
+      "In which month are grapes harvested and 'crushed' in Napa Valley?",
+    choices: ["August", "September", "October", "November", "December"],
+    answer: "October"
+  }
+];
