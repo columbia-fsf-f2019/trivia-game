e27848b3493c82c53c622890c304edf70974a518
hycoding51 <yanghuan.bnu@gmail.com>
2019-11-23 16:47:58 -0500

    add timer, display the question and add reaction to the clicked answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,16 +15,34 @@ var questions = [
     D: "Keeper",
     correctAsw: "B"
   },
-  (question3 = {
+  {
     question: "Who is Fluffy?",
     A: "Hagrid's dragon",
     B: "A three-headed dog",
     C: "Hermione's cat",
     D: "Harry's owl",
     correctAsw: "B"
-  })
+  }
 ];
-$(".timer").setInterval(() => {}, 1000);
+var time = 20;
+var intervalId;
+var correctNum;
+var wrongNum;
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  $(".timer").html("<h1>" + time + "</h1>");
+  time--;
+  if (time === 0) {
+    alert("time up");
+    clearInterval(intervalId);
+    time = 20;
+  }
+}
+run();
 function display(q) {
   $(".questions").append("<p>" + q.question + "</p>");
   $(".questions").append("<p>A: " + q.A + "</p>");
@@ -33,6 +51,42 @@ function display(q) {
   $(".questions").append("<p>D: " + q.D + "</p>");
 }
 
-for (i = 0; i < questions.length; i++) {
-  display(questions[i]);
+// for (var i = 0; i < questions.length; i++) {
+// display(questions[i]);
+var stem = $("<h3>").text(questions[0].question);
+$(".questions").append(stem);
+var choices = [];
+var choiceA = $("<button>")
+  .addClass("A")
+  .text(questions[0].A);
+choices.push(choiceA);
+var choiceB = $("<button>")
+  .addClass("B")
+  .text(questions[0].B);
+choices.push(choiceB);
+var choiceC = $("<button>")
+  .addClass("C")
+  .text(questions[0].C);
+choices.push(choiceC);
+var choiceD = $("<button>")
+  .addClass("D")
+  .text(questions[0].D);
+choices.push(choiceD);
+console.log(choices);
+for (var j = 0; j < choices.length; j++) {
+  var choiceLine = $("<h4>").append(choices[j]);
+  $(".questions").append(choiceLine);
 }
+
+$("button").on("click", function() {
+  var choice = $(this).attr("class");
+  console.log(choice);
+  if (choice === questions[0].correctAsw) {
+    $(".reaction").text("YOU ARE CORRECT!");
+    correctNum++;
+  } else {
+    $(".reaction").text("YOU ARE WRONG!");
+    wrongNum++;
+  }
+});
+// }
